958f8dabcdd0acc2e0893ade3cbce044792adc4d
Sergio Lozada <salozada@indracompany.com>
2022-07-15 11:55:20 -0500

    pruebas unitarias form sencillo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,64 @@
 import { TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, FormsModule, ReactiveFormsModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('debe crear la aplicación', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pruebas-unitarias'`, () => {
+  it('Debe retornar formulario invalido', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pruebas-unitarias');
+    fixture.detectChanges();
+
+    const email = app.form.controls['email'];
+    email.setValue('sergiolozada030@gmail.com');
+
+    expect(app.form.invalid).toBeTrue();
   });
 
-  it('should render title', () => {
+  it('Debe retornar formulario valido', () => {
     const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pruebas-unitarias app is running!');
+
+    const email = app.form.controls['email'];
+    const password = app.form.controls['password'];
+
+    email.setValue('sergiolozada030@gmail.com');
+    password.setValue('12345');
+
+    expect(app.form.valid).toBeTrue();
+  });
+
+  it('Debe Actualizar datos de usuario', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    fixture.detectChanges();
+
+    const email = app.form.controls['email'];
+    const password = app.form.controls['password'];
+
+    email.setValue('sergiolozada030@gmail.com');
+    password.setValue('12345');
+
+    const btnElement = fixture.debugElement.query(By.css('button.btn'));
+    btnElement.nativeElement.click();
+
+    const dataSession = { user: 1 };
+
+    expect(app.isCheck).toEqual(dataSession);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'pruebas-unitarias';
+export class AppComponent implements OnInit {
+  title = 'Ejercicio #1 Unit Testing Angular';
+  subTitle =
+    'Estamos creando este ejercicio para comenzar a aprender sobre pruebas unitarias en componentes';
+  form: FormGroup = new FormGroup({});
+  isCheck: any;
+
+  constructor(public fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  sendLogin(): void {
+    this.isCheck = { user: 1 };
+  }
 }
